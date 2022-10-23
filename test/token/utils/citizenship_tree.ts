bef85a135bc0ce188fe5152c48b511a8afd1f9e0
yuriarthf <yuriarthf@protonmail.com>
2022-10-23 12:59:09 +0700

    feat: citizenship merkle tree class

---
+++ test/token/utils/citizenship_tree.ts
@@ -0,0 +1,60 @@
+// citizenship_tree.ts: Implements CitizenshipTree class to be used to
+// generate merkle roots to be set for the CitizenshipPromoERC1155 contract
+
+// Import MerkleTree class to build the citizenship merkle tree
+import { MerkleTree } from "merkletreejs";
+
+// Ethers for keccak256 hash
+import ethers from "ethers";
+
+// Import types
+import type { Address } from "../../types";
+
+/**
+ * @dev Used to get the privileged citizenship merkle roots (silver and gold),
+ *      to be set in the CitizenshipPromoERC1155 contract.
+ */
+export class CitizenshipTree {
+  readonly citizenshipTree: MerkleTree;
+  readonly accounts: Address[];
+
+  /**
+   * @dev Build citizenship merkle tree
+   * @param accounts Privileged accounts
+   */
+  constructor(accounts: Address[]) {
+    this.accounts = accounts;
+    const leaves = accounts.map((address) => ethers.utils.keccak256(address));
+    this.citizenshipTree = new MerkleTree(leaves, ethers.utils.keccak256);
+  }
+
+  /**
+   * @dev Get citizenship merkle tree leaves (privileged addresses)
+   */
+  get leaves() {
+    return this.accounts.map((address) => ethers.utils.keccak256(address));
+  }
+
+  /**
+   * @dev Get citizenship merkle tree root
+   */
+  get root() {
+    return this.citizenshipTree.getHexRoot();
+  }
+
+  /**
+   * @dev Get merkle proofs for a given leaf
+   * @param leaf Keccak256 hash of a citizenship merkle tree account
+   */
+  proofs(leaf: string): string[] {
+    return this.citizenshipTree.getHexProof(leaf);
+  }
+
+  /**
+   * @dev Get proofs for the leaf in a particular index
+   * @param index Index of the leaf to get the proof
+   */
+  proofsFromIndex(index: number): string[] {
+    return this.proofs(this.leaves[index]);
+  }
+}
